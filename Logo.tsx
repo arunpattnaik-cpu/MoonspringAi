47237f15335ab95dc50ccaa6abc39fb7989e2b91
Arun <arun.pattnaik@moonspringai.com>
2026-02-17 20:57:04 +0000

    Add files via upload

---
+++ Logo.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "../logo/logo.png"; // make sure this path is correct
+
+interface LogoProps {
+  className?: string;
+}
+
+export const Logo: React.FC<LogoProps> = ({
+  className = "h-20 w-auto",
+}) => {
+  return (
+    <div className="flex items-center justify-center select-none group">
+      <img
+        src={logo}
+        alt="Moonspring AI"
+        className={`${className} object-contain transition-all duration-500 group-hover:scale-105`}
+      />
+    </div>
+  );
+};
